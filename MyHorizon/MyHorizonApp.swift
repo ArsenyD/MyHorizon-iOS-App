066eae489dd13105448d82472df46aab94237136
Arseny <arsdunaev@icloud.com>
2025-01-04 15:36:03 +0300

    Initial Commit

---
+++ MyHorizon/MyHorizonApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyHorizonApp.swift
+//  MyHorizon
+//
+//  Created by Арсений Дунаев on 4/1/25.
+//
+
+import SwiftUI
+
+@main
+struct MyHorizonApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
