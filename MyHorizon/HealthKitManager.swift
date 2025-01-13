4e324c8986d313b0b4d120017797e19b416c45e4
Arseny <arsdunaev@icloud.com>
2025-01-13 23:42:55 +0300

    Add logging to HealthKitManager

--- MyHorizon/HealthKitManager.swift
+++ MyHorizon/HealthKitManager.swift
@@ -3,25 +3,31 @@ import HealthKit
 import OSLog
 
 
-//private let logger = Logger(subsystem: "com.ArsenyD.MyHorizon", category: "HealthKitManager")
+private let logger = Logger(subsystem: "com.ArsenyD.MyHorizon", category: "HealthKitManager")
 
 @Observable
 class HealthKitManager {
     var healthStore: HKHealthStore?
-    let requestedHKTypes: Set = [
+    private let requestedHKTypes: Set = [
         HKQuantityType.workoutType()
     ]
     
     init() {
+        logger.log("initializing HealthKitManager")
+        
         if HKHealthStore.isHealthDataAvailable() {
             self.healthStore = HKHealthStore()
+            logger.log("Health data is available on the current device. Created HKHealthStore.")
             
             Task {
                 do {
+                    logger.log("Requesting authorization for health data.")
                     try await healthStore?.requestAuthorization(toShare: [], read: requestedHKTypes)
                 } catch {
                     fatalError("Authorization Request Failed")
                 }
+                
+                logger.log("Successfully requested access to health data.")
             }
             
         } else {
